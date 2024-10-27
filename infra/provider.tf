a73214c5d30e630c6e93e579861aed62d302861a
Itzmuzzy <mustafa2.mirreh@live.uwe.ac.uk>
2024-10-27 12:22:12 +0000

    Added remote backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -13,3 +13,12 @@ provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "ecs-project-bucket"
+    key            = "terraform/state/ecs.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+  }
+}
